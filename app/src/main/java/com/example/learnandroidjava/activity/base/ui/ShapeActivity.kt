11d84c93a9b343ef979a862c474ad907c56c620b
MiniOcean404 <1037306928@qq.com>
2024-04-10 20:58:55 +0800

    feature: 课件

---
+++ app/src/main/java/com/example/learnandroidjava/activity/base/ui/ShapeActivity.kt
@@ -0,0 +1,15 @@
+package com.example.learnandroidjava.activity.base.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import com.example.learnandroidjava.databinding.ActivityShapeBinding
+
+class ShapeActivity : AppCompatActivity() {
+    private val binding: ActivityShapeBinding = ActivityShapeBinding.inflate(layoutInflater)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
